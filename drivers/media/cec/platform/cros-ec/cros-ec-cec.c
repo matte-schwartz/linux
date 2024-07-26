298bd1f6d59c6d69e0afd7181de2361c3687ec3c
Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
2024-07-26 23:51:33 +0300

    media: platform: cros-ec: Add AMD Lilac to the match table
    Original commit: d03c74b3643b ("add missing lilac chromeos entry")
    
    Co-developed-by: Pierre-Loup A. Griffais <pgriffais@valvesoftware.com>
    Signed-off-by: Pierre-Loup A. Griffais <pgriffais@valvesoftware.com>
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- drivers/media/cec/platform/cros-ec/cros-ec-cec.c
+++ drivers/media/cec/platform/cros-ec/cros-ec-cec.c
@@ -295,6 +295,7 @@ struct cec_dmi_match {
 };
 
 static const char *const port_b_conns[] = { "Port B", NULL };
+static const char *const port_c_conns[] = { "Port C", NULL };
 static const char *const port_db_conns[] = { "Port D", "Port B", NULL };
 static const char *const port_ba_conns[] = { "Port B", "Port A", NULL };
 static const char *const port_d_conns[] = { "Port D", NULL };
@@ -326,6 +327,8 @@ static const struct cec_dmi_match cec_dmi_match_table[] = {
 	{ "Google", "Taranza", "0000:00:02.0", port_db_conns },
 	/* Google Dexi */
 	{ "Google", "Dexi", "0000:00:02.0", port_db_conns },
+	/* AMD Lilac */
+	{ "AMD", "Lilac", "0000:06:00.0", port_c_conns },
 };
 
 static struct device *cros_ec_cec_find_hdmi_dev(struct device *dev,
