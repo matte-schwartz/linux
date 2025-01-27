1b19910b3a813c4bce155611d40a6fce9158e89e
André Almeida <andrealmeid@igalia.com>
2025-01-27 16:59:39 -0300

    [FROM-UPSTREAM] drm/atomic: Let drivers decide which planes to async flip
    Currently, DRM atomic uAPI allows only primary planes to be flipped
    asynchronously. However, each driver might be able to perform async
    flips in other different plane types. To enable drivers to set their own
    restrictions on which type of plane they can or cannot flip, use the
    existing atomic_async_check() from struct drm_plane_helper_funcs to
    enhance this flexibility, thus allowing different plane types to be able
    to do async flips as well.
    
    Create a new parameter for the atomic_async_check(), `bool flip`. This
    parameter is used to distinguish when this function is being called from
    a plane update from a full page flip.
    
    In order to prevent regressions and such, we keep the current policy: we
    skip the driver check for the primary plane, because it is always
    allowed to do async flips on it.
    
    Signed-off-by: André Almeida <andrealmeid@igalia.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Reviewed-by: Christopher Snowhill <chris@kode54.net>
    Tested-by: Christopher Snowhill <chris@kode54.net>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250127-tonyk-async_flip-v12-1-0f7f8a8610d3@igalia.com
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    (cherry picked from commit fd40a63c63a1)
    Signed-off-by: André Almeida <andrealmeid@igalia.com>

--- drivers/gpu/drm/vc4/vc4_plane.c
+++ drivers/gpu/drm/vc4/vc4_plane.c
@@ -1450,7 +1450,7 @@ static void vc4_plane_atomic_async_update(struct drm_plane *plane,
 }
 
 static int vc4_plane_atomic_async_check(struct drm_plane *plane,
-					struct drm_atomic_state *state)
+					struct drm_atomic_state *state, bool flip)
 {
 	struct drm_plane_state *new_plane_state = drm_atomic_get_new_plane_state(state,
 										 plane);
