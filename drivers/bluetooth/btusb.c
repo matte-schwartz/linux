ad74bd8823edbe5bb57180c267cfd1bcd77e8d32
Rolando Roca <rolandor@valvesoftware.com>
2025-02-10 09:54:33 -0800

    [FOR-UPSTREAM] Adding new Quectel vid/pid
    [from commit a29050b4 by @zhongjun.yu]
    
    (cherry picked from commit a58e3d66757dfec2a2a3826270d6adaa1ad29d87)

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -312,6 +312,8 @@ static const struct usb_device_id quirks_table[] = {
 						     BTUSB_WIDEBAND_SPEECH },
 	{ USB_DEVICE(0x0489, 0xe0d0), .driver_info = BTUSB_QCA_WCN6855 |
 						     BTUSB_WIDEBAND_SPEECH },
+	{ USB_DEVICE(0x2c7c, 0x0137), .driver_info = BTUSB_QCA_WCN6855 |
+						     BTUSB_WIDEBAND_SPEECH },
 	{ USB_DEVICE(0x10ab, 0x9108), .driver_info = BTUSB_QCA_WCN6855 |
 						     BTUSB_WIDEBAND_SPEECH },
 	{ USB_DEVICE(0x10ab, 0x9109), .driver_info = BTUSB_QCA_WCN6855 |
