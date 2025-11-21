da3d5860ea5ba415752aeeda5a84967ebedb40b9
Joshua Aberback <joshua.aberback@amd.com>
2025-11-21 15:46:17 -0500

    [FROM-UPSTREAM] drm/amd/display: Defer transitions from minimal state to final state
    [Why]
    In non-seamless pipe transitions, it can take several frames to process
    a single flip. One of the reasons is the 2-step transition implementation
    where first the minimal transition state is applied, then the final state
    is applied, all within the same flip. This delay is noticeable to the user
    in some video playback scenarios, which makes for a bad user experience.
    
    [How]
     - in applicable non-seamless cases, complete the flip with the minimal
       state applied, start a counter, and create all new contexts as minimal
     - if another pipe transition occurs while counting, reset the counter
     - when the counter finishes, promote the current flip to a full update
       and restore creation of optimized contexts
     - when creating minimal states from new context, apply stream updates
    
    Reviewed-by: Aric Cyr <aric.cyr@amd.com>
    Signed-off-by: Joshua Aberback <joshua.aberback@amd.com>
    Signed-off-by: Chenyu Chen <chen-yu.chen@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    (cherry picked from commit 24ddca9a3af1934194765747f5175afd30aa86e2
    resolving conflicts)
    Upstream-url: git@ssh.gitlab.freedesktop.org:agd5f/linux.git
    Signed-off-by: Robert Beckett <bob.beckett@collabora.com>

--- drivers/gpu/drm/amd/display/dc/dc.h
+++ drivers/gpu/drm/amd/display/dc/dc.h
@@ -69,6 +69,8 @@ struct dmub_notification;
 #define MAX_HOST_ROUTERS_NUM 3
 #define MAX_DPIA_PER_HOST_ROUTER 2
 
+#define NUM_FAST_FLIPS_TO_STEADY_STATE 20
+
 /* Display Core Interfaces */
 struct dc_versions {
 	const char *dc_ver;
@@ -244,6 +246,9 @@ struct dc_check_config {
 	 */
 	unsigned int max_optimizable_video_width;
 	bool enable_legacy_fast_update;
+
+	bool deferred_transition_state;
+	unsigned int transition_countdown_to_steady_state;
 };
 
 struct dc_caps {
@@ -1099,6 +1104,9 @@ struct dc_debug_options {
 	bool force_subvp_df_throttle;
 	uint32_t acpi_transition_bitmasks[MAX_PIPES];
 	bool enable_pg_cntl_debug_logs;
+	uint32_t custom_psp_footer_size;
+	bool disable_deferred_minimal_transitions;
+	unsigned int num_fast_flips_to_steady_state_override;
 };
 
 
