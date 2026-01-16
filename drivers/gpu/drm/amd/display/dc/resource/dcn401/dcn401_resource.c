2cb8d7ad7b0f28273580c63d912a5c7c6c936eb3
Melissa Wen <mwen@igalia.com>
2026-01-16 13:26:01 -0300

    [FROM-ML] drm/amd/display: fix screen blinking when exits SubVP to MPO
    Whenever transitioning from SubVP to MPO in a high refresh rate and high
    resolution display, for example, 4k@120Hz or higher, screen blinks as a
    white flash. On gamescope, it happens every time the cursor disappears
    because it stops composing in a single plane and returns using primary +
    one/two overlay planes. According to [1] SubVP + MPO isn't supported and
    the white flash seems to occur exactly when exiting SubVP.
    
    It happens with DCN321 and DCN401, at least. As there is no transition
    from SubVP to MPO if SubVP is disabled (ofc), there is no blink when
    moving from single plane to multiple planes. Check [2] for more details.
    
    [1] https://gitlab.freedesktop.org/agd5f/linux/-/commit/14eb72ff580d
    [2] https://gitlab.freedesktop.org/drm/amd/-/issues/4877
    
    Signed-off-by: Melissa Wen <mwen@igalia.com>
    Link: https://lore.kernel.org/amd-gfx/20260116162741.89743-1-mwen@igalia.com/
    Signed-off-by: Nícolas F. R. A. Prado <nfraprado@collabora.com>

--- drivers/gpu/drm/amd/display/dc/resource/dcn401/dcn401_resource.c
+++ drivers/gpu/drm/amd/display/dc/resource/dcn401/dcn401_resource.c
@@ -708,7 +708,7 @@ static const struct dc_debug_options debug_defaults_drv = {
 		}
 	},
 	.use_max_lb = true,
-	.force_disable_subvp = false,
+	.force_disable_subvp = true,
 	.exit_idle_opt_for_cursor_updates = true,
 	.using_dml2 = true,
 	.using_dml21 = true,
